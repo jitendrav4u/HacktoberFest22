5047190b7bccb9ec7df3bd3fd4fa397d7ebbb6d3
Arunim Singhal <75484060+Asuna-AS@users.noreply.github.com>
2022-10-01 23:58:13 +0530

    Create Maximum_Product_Subarray.cpp

---
+++ Maximum_Product_Subarray.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+     int r=nums[0];
+     int maxi=r;
+     int mini=r;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]<0)
+            {
+                int temp=maxi;
+                maxi=mini;
+                mini=temp;
+            }
+            maxi=max(nums[i],nums[i]*maxi);
+            mini=min(nums[i],nums[i]*mini);
+            r=max(r,maxi);
+        }
+        return r;
+    }
+};
+
+/*
+Best Possible
+Tc: O(n)
+Sc: O(1)
+*/
+  
