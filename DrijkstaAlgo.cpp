75ace10bdbdeba8fe638a6347eff0dad437430b9
vaishnavi1011 <80156787+vaishnavi1011@users.noreply.github.com>
2022-10-02 13:26:21 +0530

    Create DrijkstaAlgo.cpp

---
+++ DrijkstaAlgo.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n,m,source;
+    cin>>n>>m;
+    vector<pair<int,int>>g[m+1];
+    int a,b,wt;
+    for(int i=0;i<m;i++){
+        cin>>a>>b>>wt;
+        g[a].push_back(make_pair(b,wt));
+        g[b].push_back(make_pair(a,wt));
+    }
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    vector<int>distTo(n+1,INT_MAX);
+    distTo[source]=0;
+    pq.push(make_pair(0,source));
+    while(!pq.empty()){
+        int dist=pq.top().first;
+        int prev=pq.top().second;
+        pq.pop();
+        vector<pair<int,int>>::iterator it;
+        for(it=g[prev].begin();it!=g[prev].end();it++){
+            int next=it->first;
+            int nextDist=it->second;
+            if(distTo[next]>distTo[prev]+nextDist){
+                distTo[next]=distTo[prev]+nextDist;
+                pq.push(make_pair(distTo[next],next));
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<distTo[i]<<" ";
+    }
+}
